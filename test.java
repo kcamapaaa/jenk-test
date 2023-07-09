c877cda5be01d3e0fae56fdcd010801647fa52cb
kcamapaaa <112584914+kcamapaaa@users.noreply.github.com>
2023-07-10 00:44:40 +0300

    Update test.java

--- test.java
+++ test.java
@@ -1,5 +1,5 @@
 public class Test {
   public static void main(String...args) {
-    System.out.println(Hello test file from github for Jenkins!);
+    System.out.println("Hello test file from github for Jenkins!");
     }
 }
